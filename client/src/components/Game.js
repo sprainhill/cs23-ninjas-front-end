a36ecd09b7a565bc74e1d660c96fca54ce3a2631
Rory Murray <rory.christian.murray@gmail.com>
2019-11-18 17:51:45 -0500

    Create route for testing Map

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { axiosWithAuth } from '../utilities/axiosWithAuth.js';
+import Map from './Map';
 
 const Game = ({ logout }) => {
   const [gameInfo, setGameInfo] = useState({});
