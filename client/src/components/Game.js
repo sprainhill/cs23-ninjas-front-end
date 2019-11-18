c1d7bc559639ff0405542e234a15f68d4dcdf5d8
Michael VanSleen <mike.vansleen@gmail.com>
2019-11-18 16:25:31 -0700

    Game UI is scaffolded out, some styling done

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { axiosWithAuth } from '../utilities/axiosWithAuth.js';
 
+import fakeMap from '../assets/images/map-sample.png';
+
 const Game = ({ logout }) => {
   const [gameInfo, setGameInfo] = useState({});
 
@@ -39,26 +41,51 @@ const Game = ({ logout }) => {
   }, []);
 
   return (
-    <div>
-      <h2>Welcome to the Tunnels {gameInfo.name}!</h2>
-      <h3>Room: {gameInfo.title}</h3>
-      <p>{gameInfo.description}</p>
-      {gameInfo.error_msg && <p>{gameInfo.error_msg}</p>}
-      <form onSubmit={e => move(e, direction)}>
-        <input
-          onChange={handleChange}
-          type="text"
-          placeholder="Type commands here:"
-          value={direction}
-        />
-      </form>
-
-      <h4>Players In Room:</h4>
-      <ul>
-        {gameInfo.players &&
-          gameInfo.players.map(player => <li key={player}>{player}</li>)}
-      </ul>
-      <button onClick={logout}>Log Out</button>
+    <div className="game-container">
+      <div className="column side">
+        <div className="box map">
+          <h3>{gameInfo.title}</h3>
+          <p>
+            <span>Exits:</span> West, North, South
+          </p>
+          <img src={fakeMap} />
+          <p>Items In Room:</p>
+        </div>
+        <div className="box player-info">
+          <h3>{gameInfo.name}</h3>
+          <hr />
+        </div>
+      </div>
+      <div className="column middle">
+        <div className="box game-info">
+          <p className="room-info-text">{gameInfo.title}</p>
+          <p className="room-info-text">{gameInfo.description}</p>
+          {gameInfo.error_msg && <p>{gameInfo.error_msg}</p>}
+        </div>
+        <div className="box command-input">
+          <form onSubmit={e => move(e, direction)}>
+            <input
+              onChange={handleChange}
+              type="text"
+              placeholder="Type commands here:"
+              value={direction}
+            />
+          </form>
+        </div>
+      </div>
+      <div className="column side">
+        <div className="box players-online">
+          <h3>Players In Room</h3>
+          <hr />
+          {gameInfo.players &&
+            gameInfo.players.map(player => <p key={player}>{player}</p>)}
+        </div>
+        <div className="box chat">
+          <h3>Chat</h3>
+          <hr />
+          <button onClick={logout}>Log Out</button>
+        </div>
+      </div>
     </div>
   );
 };
