ed609b6c972220bed0df11559cc23be9b4e6ed8b
Michael VanSleen <mike.vansleen@gmail.com>
2019-11-19 14:56:07 -0700

    Switched api url to actual deployed url

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -17,11 +17,9 @@ const Login = ({ history, setIsLoggedIn, setIsLoading }) => {
   const handleSubmit = e => {
     e.preventDefault();
     console.log('user from login handlesubmit', user);
-    const proxy = 'https://cors-anywhere.herokuapp.com/';
-    const url = 'https://lambda-mud-test.herokuapp.com/';
     setIsLoading(true);
     axios
-      .post(`${proxy}${url}api/login/`, user)
+      .post(`${process.env.REACT_APP_BASE_URL}api/login/`, user)
       .then(res => {
         localStorage.setItem('token', res.data.key);
         setIsLoading(false);
